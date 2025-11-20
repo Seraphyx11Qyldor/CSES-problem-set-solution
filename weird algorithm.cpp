7e8a82e13872b724893cc0c50a72c87fbf50e06b
Seraphyx11Qyldor <suborno311@gmail.com>
2025-11-21 01:04:37 +0600

    Add files via upload

---
+++ weird algorithm.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    long long n;
+    cin >> n;
+
+    cout << n;
+    while (n != 1)
+    {
+        if (n % 2 == 0)
+            n /= 2;
+        else
+            n = 3 * n + 1;
+        cout << " " << n;
+    }
+    cout << "\n";
+}
