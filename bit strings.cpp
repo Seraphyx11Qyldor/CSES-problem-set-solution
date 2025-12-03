3a5c1f1b0854e6c881dc2479cb766d5055767686
Seraphyx11Qyldor <suborno311@gmail.com>
2025-12-03 15:00:54 +0600

    Add files via upload

--- bit strings.cpp
+++ bit strings.cpp
@@ -1,12 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
+typedef long long ll;
+int MOD = 1e9 + 7;
 
-int main() {
+ll power(ll base, ll expo)
+{
+    ll result = 1;
+    while (expo)
+    {
+        if (expo & 1ll)
+        {
+            result = (result * base) % MOD;
+        }
+
+        base = (base * base) % MOD;
+        expo >>= 1ll;
+    }
+
+    return result;
+}
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(NULL);
 
+    ll n;
 
+    cin >> n;
 
-    
-    
+    cout << power(2, n) << "\n";
 }
